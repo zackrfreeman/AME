79d54a01a1a3fec020972c8970bc0697b62aa5c5
Zack Freeman <zackrfreeman@gmail.com>
2025-05-07 18:11:03 -0400

    S

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -23,6 +23,19 @@ function ariaExpanded() {
     }
 }
 
+// This script adds a class to the body after scrolling 100px
+// and we used these body.scroll styles to create some on scroll 
+// animations with the navbar
+
+document.addEventListener('scroll', (e) => { 
+    const scroll = document.documentElement.scrollTop;
+    if(scroll >= 100){
+document.querySelector('body').classList.add('scroll')
+    } else {
+    document.querySelector('body').classList.remove('scroll')
+    }
+});
+
 // mobile nav toggle code
 const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
     for (const item of dropDowns) {
@@ -31,14 +44,4 @@ const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropd
     }
     item.addEventListener('click', onClick)
     }
-
-// after scrolling down 100px, add .scroll class to the #cs-navigation
-document.addEventListener('scroll', (e) => { 
-    const scroll = document.documentElement.scrollTop;
-    if(scroll >= 100){
-document.querySelector('#cs-navigation').classList.add('scroll')
-    } else {
-    document.querySelector('#cs-navigation').classList.remove('scroll')
-    }
-});
                             
